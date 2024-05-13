f50c155f0c25eb6e998c1a7c5f364a661b3c737d
M.SY <80853826+khalidouafi@users.noreply.github.com>
2024-05-13 18:00:05 +0000

    Update LoginForm.php

--- Seynabou Dia/LoginForm.php
+++ Seynabou Dia/LoginForm.php
@@ -1,32 +1,3 @@
-<?php
-require_once 'DbConnect.php';
-
-
-$db = new DbConnect();
-$conn = $db->connect();
-
-if(isset($_POST['UserSave'])) {
-    $lastName = $_POST['LastName'];
-    $firstName = $_POST['FirstName'];
-    $phone = $_POST['Phone'];
-    $city = $_POST['City'];
-    $address = $_POST['Address'];
-
-    $stmt = $conn->prepare("INSERT INTO UserTable (LastName, FirstName, Phone, City, Address) VALUES (:lastName, :firstName, :phone, :city, :address)");
-    $stmt->bindParam(':lastName', $lastName);
-    $stmt->bindParam(':firstName', $firstName);
-    $stmt->bindParam(':phone', $phone);
-    $stmt->bindParam(':city', $city);
-    $stmt->bindParam(':address', $address);
-
-   
-    $stmt->execute();
-}
-
-$stmt = $conn->query("SELECT * FROM UserTable");
-$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
 <!DOCTYPE html>
 
 <head>
@@ -39,39 +10,40 @@ $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     <h1>SaveUSERS</h1>
     <form method="POST" action="">
-        <label for="LastName">LastName:</label><br>
-        <input type="text" id="LastName" name="LastName" required><br>
-        <label for="FirstName">FirstName:</label><br>
-        <input type="text" id="FirstName" name="FirstName" required><br>
-        <label for="Phone">Phone:</label><br>
-        <input type="text" id="Phone" name="Phone"><br>
-        <label for="City">City:</label><br>
-        <input type="text" id="City" name="City"><br>
-        <label for="Address"><Address></Address>Address:</label><br>
-        <textarea id="Address" name="Address"></textarea><br>
-        <button type="submit" name="UserSave">UserSave</button>
+        <label for="Login">USER LOGIN:</label><br>
+        <input type="text" id="Login" name="Login" required><br>
+        <label for="Password">PASSWORD:</label><br>
+          <input type="password" id="Password" name="Password" required><br>
+       <button type="submit" name="Connexion">CONNEXION</button>
     </form>
-    <h1>ShowContacts</h1>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>LastName</th>
-            <th>FirstName</th>
-            <th>Phone</th>
-            <th>City</th>
-            <th>Address</th>
-        </tr>
-        <?php foreach($contacts as $contact): ?>
-        <tr>
-            <td><?= $contact['Id'] ?></td>
-            <td><?= $contact['LastName'] ?></td>
-            <td><?= $contact['FirstName'] ?></td>
-            <td><?= $contact['Phone'] ?></td>
-            <td><?= $contact['City'] ?></td>
-            <td><?= $contact['Address'] ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
+   <?php
+require_once 'DbConnect.php';
+
+
+$db = new DbConnect();
+$conn = $db->connect();
+
+if(isset($_POST['Connexion'])) {
+    $log = $_POST['Login'];
+    $pass = $_POST['Password'];
+  
+    $stmt = $conn->prepare("Select * from UserTable where :Login and :Password)");
+    $stmt->bindParam(':Login', $log);
+    $stmt->bindParam(':Password', $firstName);
+     
+    $stmt->execute();
+$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if($user){
+        header("location: welcome.php");
+}
+    else{
+       header("location: LoginErr.php");
+    }
+
+}
+?>
+
+      
 
 <!DOCTYPE html>
 
